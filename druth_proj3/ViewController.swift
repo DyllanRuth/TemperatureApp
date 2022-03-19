a5f2106b207905ca4e17684f3c7bc93c8cbd9163
Dyllanruth <Dyllan.ruth@gmail.com>
2022-03-19 14:53:08 -0500

    test

--- druth_proj3/ViewController.swift
+++ druth_proj3/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Variables
+    @IBOutlet weak var CelciusInput: UITextField!
+    @IBOutlet weak var FarenheitInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Default Values
+        CelciusInput.text = "0.0"
+        FarenheitInput.text = "32.0"
     }
-
+    //Functions
+    @IBAction func C2F(_ sender: Any) {
+        view.endEditing(true)
+        if let tInCx = Float(CelciusInput.text!){
+            let tInFx = (tInCx*9.0/5.0)+32.0
+            FarenheitInput.text = String(format: "%0.1f", tInFx)
+        } else {
+            //show popup window
+        }
+    }
+    @IBAction func F2C(_ sender: Any) {
+        view.endEditing(true)
+        if let tInTx = Float(FarenheitInput.text!){
+            let tInPx = (tInTx-32.0)*5.0/9.0
+            print(tInPx)
+            CelciusInput.text = String(format: "%0.1f", tInPx)
+        } else {
+            //show popup window
+        }
+    }
+    
 
 }
 
